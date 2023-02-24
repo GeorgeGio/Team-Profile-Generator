3dd3fb0b1cfac9f80e469dcf9bcdb79e5798e26a
George <georgeoustapas@gmail.com>
2023-02-24 19:35:08 +0000

    updated the library files of the team

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
-const fs = require("fs");
+const fs = require("fs/promises");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -12,4 +12,6 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+
+let team = [];
 
