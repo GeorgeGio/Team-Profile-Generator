03734b22455f773f93992a7a9e06e1ac859a1f5f
George <georgeoustapas@gmail.com>
2023-02-27 20:49:00 +0000

    added an output folder and include fs/promises method

--- index.js
+++ index.js
@@ -14,4 +14,51 @@ const render = require("./src/page-template.js");
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
 let team = [];
+startApplication();
 
+async function startApplication() {
+    
+    
+    let {member, name,id,email,github,school,officeNumber} = await inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'member',
+            message: "What type of role you adding?",
+            choices: ["Manager", "Engineer", "Intern"],
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: "what is the managers name ?",
+           
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "what is the managers id ?",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "what is the managers email ? ",
+        },
+        {
+            type: 'list',
+            name: 'member',
+            message: "What type of role you adding?",
+            choices: [ "Engineer", "Intern"],
+        },
+
+        // when : input => {
+
+        // }
+        
+        
+
+    ]);
+
+    
+    let hmtlDoc = render(team)
+    await fs.writeFile(outputPath,hmtlDoc)
+}
